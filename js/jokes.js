666c609c559d75a6627af1d80f6e4221911ddab7
Ivette <ivparejo@gmail.com>
2021-01-18 18:34:08 +0100

    Data showed on the website and console.log

--- js/jokes.js
+++ js/jokes.js
@@ -1,28 +1,20 @@
-// Public URL API
-const url = 'https://icanhazdadjoke.com/';
+const url = 'https://icanhazdadjoke.com/'; // Public URL API
 
+function generate(){
 //We use in Javascript the method fetch() in order to POST/GET/PUT/PATCH/DELETE data from the API provided. This method returns a PROMISE
-fetch(url , {
-    // The GET operation is defined because we want to SEE the data from the API
-    method: 'GET',
-    headers: {
-        'Accept': 'application/json',
+    fetch(url , {
+        method: 'GET',   // The GET operation is defined because we want to SEE the data from the API
+        headers: {
+            'Accept': 'application/json',
+        }
+    })      
+        .then(response => response.json()) //The .THEN() method returns an OBJECT 
+        // ******** EXERCICE 1 ********
+        .then(data => {
+            console.log(data);
+            document.getElementById("content").innerHTML= `${data}`
+        })
     }
-})      
-
-//The .THEN() method returns an OBJECT 
-.then(response => response.json())
-
-// ******** EXERCICE 1 ********
- .then(data => console.log(data)) 
-
-// ******** EXERCICE 2 ********
-// .then(json => {
-//     let data = JSON.stringify(json)
-//     document.getElementById('demo').innerHTML = data;
-//   });
-
-
-//The .CATCH() methodn is used to see if there's any problem
-// .catch(error => console.log("error!"))
+    //The .CATCH() method is used to see if there's any problem
+    // .catch(error => console.log("error!"))
 
